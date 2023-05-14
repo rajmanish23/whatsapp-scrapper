fe56a1c4674d0e09a30bcdf5050d4a5fad6ad953
Raj Manish <katrajmanish23@gmail.com>
2023-05-14 15:22:18 +0530

    code opens zoom links now.

--- main.py
+++ main.py
@@ -3,13 +3,16 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import wait
 from selenium.webdriver.support import expected_conditions as ec
 
-group_name = "MREC III CSE C 2020"
+group_name = "COPIUM LAN"
 
 driver = webdriver.Chrome()
 driver.get("https://web.whatsapp.com/")
 driver.implicitly_wait(5)
+
+original_window = driver.current_window_handle
 
 WebDriverWait(driver, 60).until(
     ec.presence_of_element_located(
@@ -26,5 +29,28 @@ time.sleep(2)
 
 contact_element = driver.find_element(By.CLASS_NAME, "_8nE1Y")
 contact_element.click()
+
+time.sleep(2)
+
+
+def open_link():
+    message_elements = driver.find_elements(By.CLASS_NAME, "_11JPr")
+    latest_message = message_elements[-1].text
+    print(latest_message)
+
+    driver.switch_to.new_window('tab')
+    driver.get(latest_message)
+
+    time.sleep(5)
+    launch_meeting_btn = driver.find_element(By.CLASS_NAME, "mbTuDeF1")
+    launch_meeting_btn.click()
+
+    time.sleep(20)
+    driver.switch_to.window(original_window)
+
+
+
+for i in range(2):
+    open_link()
 
 input()
